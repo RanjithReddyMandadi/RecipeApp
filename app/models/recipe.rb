73d8176d3c507741f7a2bf991e0083752540c312
Ranjith <ran.mandadi@gmail.com>
2013-11-25 18:47:07 +0000

    initial commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,4 @@
+class Recipe < ActiveRecord::Base
+  attr_accessible :description, :ingredients, :preptime, :title
+  belongs_to :user
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipesApp::Application.config.secret_token = 'f05f2cdb2cbbaa5de95f87631570d3b9678b41c5530916130725167ccdcfd1f230092ae6e70c7e5b291315e401947e375b176c079ac22a3db3524ad96b53739f'
