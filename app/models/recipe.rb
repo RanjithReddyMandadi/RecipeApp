0576cd14b0b370bde48fb171624d8fbd29f15835
Ranjith <ran.mandadi@gmail.com>
2013-11-28 14:28:47 +0000

    add file(image) uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :description, :ingredients, :preptime, :title
+  attr_accessible :description, :ingredients, :preptime, :title, :image, :user_id
   belongs_to :user
+  mount_uploader :image, ImageUploader
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131125180411) do
+ActiveRecord::Schema.define(:version => 20131128123759) do
 
   create_table "recipes", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20131125180411) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "users", :force => true do |t|
